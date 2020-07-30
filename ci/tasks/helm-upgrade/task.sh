2eb1746f38abc879e69eaae905d62ccba6653a3f
Colin Simmons <colin.simmons@engineerbetter.com>
2020-07-30 19:05:13 +0100

    auth without gcloud

--- ci/tasks/helm-upgrade/task.sh
+++ ci/tasks/helm-upgrade/task.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
+# https://ahmet.im/blog/authenticating-to-gke-without-gcloud/
+
 set -eu
 
-echo "$KUBECONFIG_CONTENTS" > kubeconfig.yml
+echo "$KUBECONFIG_CONTENTS" > kubeconfig.json
+echo "$GOOGLE_APPLICATION_CREDENTIALS_CONTENTS" > google_creds.json
+
+export GOOGLE_APPLICATION_CREDENTIALS=google_creds.json
 
 sleep 500
