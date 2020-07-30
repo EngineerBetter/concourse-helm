974a07804504896737e26d09c525e5be16b40c92
Colin Simmons <colin.simmons@engineerbetter.com>
2020-07-30 20:34:48 +0100

    make cert locally signed instead of self signed

--- terraform/tls.tf
+++ terraform/tls.tf
@@ -1,12 +1,42 @@
 variable "web_domain" {}
 
+resource "tls_private_key" "ca" {
+  algorithm = "ECDSA"
+}
+
+resource "tls_self_signed_cert" "ca" {
+  key_algorithm     = "${tls_private_key.ca.algorithm}"
+  private_key_pem   = "${tls_private_key.ca.private_key_pem}"
+  is_ca_certificate = true
+
+  validity_period_hours = 87600
+
+  subject {
+    common_name = "ConcourseCA"
+  }
+}
+
 resource "tls_private_key" "tls_key" {
   algorithm = "ECDSA"
 }
 
-resource "tls_self_signed_cert" "web_tls" {
-  key_algorithm         = tls_private_key.tls_key.algorithm
-  private_key_pem       = tls_private_key.tls_key.private_key_pem
+resource "tls_cert_request" "cert" {
+  key_algorithm   = "${tls_private_key.cert.algorithm}"
+  private_key_pem = "${tls_private_key.cert.private_key_pem}"
+
+  dns_names = [var.web_domain]
+
+  subject {
+    common_name = var.web_domain
+  }
+}
+
+resource "tls_locally_signed_cert" "web_tls" {
+  cert_request_pem   = "${tls_cert_request.cert.cert_request_pem}"
+  ca_key_algorithm   = "${tls_private_key.ca.algorithm}"
+  ca_private_key_pem = "${tls_private_key.ca.private_key_pem}"
+  ca_cert_pem        = "${tls_self_signed_cert.ca.cert_pem}"
+
   validity_period_hours = 8760
 
   # Generate a new certificate if Terraform is run within 24
@@ -21,14 +51,10 @@ resource "tls_self_signed_cert" "web_tls" {
   ]
 
   dns_names = [var.web_domain]
-
-  subject {
-    common_name = var.web_domain
-  }
 }
 
 output "web_tls_cert" {
-  value = tls_self_signed_cert.web_tls.cert_pem
+  value = tls_locally_signed_cert.web_tls.cert_pem
 }
 
 output "web_tls_key" {
