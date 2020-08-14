c07c15c853d7efbfe7cabad47536b9f3e3d5146c
Colin Simmons <colin.simmons@engineerbetter.com>
2020-08-14 19:12:43 +0100

    deploy prometheus operator

---
+++ ci/tasks/prometheus-operator-upgrade/task.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC2140
+
+set -eu
+
+this_dir="$(cd "$(dirname "$0")" && pwd)"
+
+# shellcheck disable=SC1090
+. "${this_dir}/../helpers/functions.sh"
+
+setup_kubeconfig
+
+helm --kubeconfig=kubeconfig.json repo add stable https://kubernetes-charts.storage.googleapis.com
+
+install_helm_diff
+
+echo "Changes to be applied:"
+helm diff upgrade prometheus stable/prometheus-operator \
+  --kubeconfig=kubeconfig.json \
+  --install \
+  --namespace prometheus-operator \
+  --suppress-secrets
+
+echo "Performing upgrade"
+helm upgrade prometheus-operator stable/prometheus-operator \
+  --kubeconfig=kubeconfig.json \
+  --install \
+  --namespace prometheus-operator \
+  --create-namespace \
+  --atomic
